c15538bb921df64e7d55e6a4f31cd25f228ce9de
Vivian So <vso@thoughtworks.com>
2023-07-25 10:01:04 -0400

    Finished session that talked about polymorphism

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -3,29 +3,47 @@ class StringCalculator
     return 0 if str.empty?
     return str.to_i unless str.include?(',') || str.include?('\n')
 
-    ints = delimit_input(str)
+    ints = Delimiter.create(str).split
 
     negatives = ints.filter { |i| i.to_i < 0 }
     raise StandardError, "negatives not allowed: #{negatives.join(',')}" if negatives.length > 0
 
     ints.filter { |i| i.to_i <= 1000 }.sum { |x| x.to_i }
   end
+end
 
-  def delimit_input(str)
-    if str.include?('//')
-      custom_delimiter(str)
+class Delimiter
+  def self.create(str)
+    if str.include?('//') == true
+      CustomDelimiter.new(str)
     else
-      default_delimiter(str)
+      DefaultDelimiter.new(str)
     end
   end
+end
 
-  def custom_delimiter(str)
+class CustomDelimiter
+  attr_reader :str
+
+  def initialize(str)
+    @str = str
+  end
+
+  def split
     head, tail = str.split('\n')
     delimiter = head.sub('//', '')
     ints = tail.split(delimiter)
   end
+end
 
-  def default_delimiter(str)
+class DefaultDelimiter
+  attr_reader :str
+
+  def initialize(str)
+    @str = str
+  end
+
+  def split
     ints = str.split(/[\\n,]/)
   end
 end
