8c6c99df8c42d905a8e14cf7b82c82aeedb338ff
Vivian So <vso@thoughtworks.com>
2023-08-25 16:27:09 -0400

    Jul 25th session

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -5,38 +5,24 @@ class StringCalculator
 
     ints = Delimiter.create(str).split
 
+    ensure_positive_numbers(ints)
+
+    filter_numbers_less_than_1000(ints).sum { |x| x.to_i }
+  end
+
+  private
+  
+  def filter_numbers_less_than_1000(ints)
+    ints.filter { |i| i.to_i <= 1000 }
+  end
+
+  def ensure_positive_numbers(ints)
     negatives = ints.filter { |i| i.to_i < 0 }
     raise StandardError, "negatives not allowed: #{negatives.join(',')}" if negatives.length > 0
-
-    ints.filter { |i| i.to_i <= 1000 }.sum { |x| x.to_i }
   end
 end
 
 class Delimiter
-  def self.create(str)
-    if str.include?('//') == true
-      CustomDelimiter.new(str)
-    else
-      DefaultDelimiter.new(str)
-    end
-  end
-end
-
-class CustomDelimiter
-  attr_reader :str
-
-  def initialize(str)
-    @str = str
-  end
-
-  def split
-    head, tail = str.split('\n')
-    delimiter = head.sub('//', '')
-    ints = tail.split(delimiter)
-  end
-end
-
-class DefaultDelimiter
   attr_reader :str
 
   def initialize(str)
@@ -45,5 +31,21 @@ class DefaultDelimiter
 
   def split
     ints = str.split(/[\\n,]/)
+  end
+
+  def self.create(str)
+    if str.include?('//') == true
+      CustomDelimiter.new(str)
+    else
+      Delimiter.new(str)
+    end
+  end
+end
+
+class CustomDelimiter < Delimiter
+  def split
+    head, tail = str.split('\n')
+    delimiter = head.sub('//', '')
+    ints = tail.split(delimiter)
   end
 end
